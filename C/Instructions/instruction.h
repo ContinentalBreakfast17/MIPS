4300081e1c69f7db15f92796d9c860867aea198b
MasterOfJenkum <hunterrhollant@gmail.com>
2018-05-21 23:10:01 -0400

    template for cleaning up re-arching instruction decoding

--- C/Instructions/instruction.h
+++ C/Instructions/instruction.h
@@ -1,22 +1,59 @@
+#define OP_I_REG 	0
+#define OP_F_REG 	1
+#define OP_C_FLAG 	2
+#define OP_IM 		3
+
 typedef struct r_inst_data {
-	unsigned int 	opcode1: 	6;
-	unsigned int 	s: 			5;
-	unsigned int 	t: 			5;
-	unsigned int 	d: 			5;
-	unsigned int 	shift:		5;
 	unsigned int 	opcode2: 	6;
+	unsigned int 	shift:		5;
+	unsigned int 	d: 			5;
+	unsigned int 	t: 			5;
+	unsigned int 	s: 			5;
+	unsigned int 	opcode1: 	6;
 } R_Data;
 
 typedef struct i_inst_data {
-	unsigned int 	opcode: 	6;
-	unsigned int 	s: 			5;
-	unsigned int 	t: 			5;
 	unsigned int 	im:			16;
+	unsigned int 	t: 			5;
+	unsigned int 	s: 			5;
+	unsigned int 	opcode: 	6;
 } I_Data;
 
 typedef struct j_inst_data {
-	unsigned int 	opcode: 	6;
 	unsigned int 	address: 	26;
+	unsigned int 	opcode: 	6;
 } J_Data;
+
+typedef union {
+	unsigned int 	b;
+	R_Data 			r;
+	I_Data 			i;
+	J_Data 			j;
+} Inst_Bits;
+
+typedef struct instruction_dest {
+	unsigned int 	type;
+	unsigned int 	value;
+	unsigned int 	size;
+	unsigned int 	alignment;
+} Inst_Dest;
+
+typedef struct instruction_operand {
+	unsigned int 	type;
+	unsigned int 	value;
+	unsigned int 	size;
+} Operand;
+
+typedef struct instruction_data {
+	Operand* 		op_1;
+	Operand* 		op_2;
+	Operand* 		op_3; // only useful for MADD / MSUB :(
+	Inst_Dest* 		dest;
+	unsigned int 	opcode;
+	unsigned int 	pc;
+	unsigned int 	overflow: 	1;
+	unsigned int 	fp: 		1;
+	unsigned int 	branch: 	1;
+} Inst_Data;
 
 int decipher_instruction(unsigned int inst, P_Regs* tmp_interstage_regs, ALU_Data* alu_data);
--- C/Instructions/r_instruction.c
+++ C/Instructions/r_instruction.c
@@ -1,579 +1,82 @@
-#define bits_1_to_6   unsigned int(inst & 0xfc000000) >> 26;
-#define bits_7_to_11  unsigned int(inst & 0x03e00000) >> 21;
-#define bits_12_to_16 unsigned int(inst & 0x001f0000) >> 16;
-#define bits_17_to_21 unsigned int(inst & 0x0000f800) >> 11;
-#define bits_22_to_26 unsigned int(inst & 0x000007c0) >> 6;
-#define bits_27_to_32 unsigned int(inst & 0x0000003f);
+// TODO: branch
+// u -> overflow = 0
 
-void i_add(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_ADD;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 1;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;
-	p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
+// add, addu, and, nor, sllv, srav, srlv, sub, subu, xor, slt, sltu, movn, movz, mul
+void r_type_1(unsigned int code, Inst_Bits inst, Inst* data, unsigned int overflow) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, inst.r->rs, SZ_WORD);
+	data->op_2 = new_operand(OP_I_REG, inst.r->rt, SZ_WORD);
+	data->dest = new_dest(OP_I_REG, inst.r->rd, SZ_WORD, MEM_ALIGN_FORCE);
+	data->overflow = overflow;
 }
 
-void i_addu(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_ADD;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 0;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;
-	p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
+// clo, clz, jalr, mfc0, mtc0
+void r_type_2(unsigned int code, Inst_Bits inst, Inst* data, int branch) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, inst.r->rs, SZ_WORD);
+	data->dest = new_dest(OP_I_REG, inst.r->rd, SZ_WORD, MEM_ALIGN_FORCE);
 }
 
-void i_and(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_AND;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;
-	p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
+// sll, sra, srl
+void r_type_3(unsigned int code, Inst_Bits inst, Inst* data) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, inst.r->rs, SZ_WORD);
+	data->op_2 = new_operand(OP_IM, inst.r->shift, SZ_WORD);
+	data->dest = new_dest(OP_I_REG, inst.r->rd, SZ_WORD, MEM_ALIGN_FORCE);
+	data->overflow = overflow;
 }
 
-void i_clo(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_CLO;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
+// jr
+void r_type_4(unsigned int code, Inst_Bits inst, Inst* data) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, inst.r->rs, SZ_WORD);
 }
 
-void i_clz(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_CLZ;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
+// teq, tne, tge, tgeu, tlt, tltu
+void r_type_5(unsigned int code, Inst_Bits inst, Inst* data, unsigned int overflow) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, inst.r->rs, SZ_WORD);
+	data->op_2 = new_operand(OP_I_REG, inst.r->rt, SZ_WORD);
+	data->overflow = overflow;
 }
 
-void i_div(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_DIV;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 1;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
+// mfhi, mflo
+void r_type_6(unsigned int code, Inst_Bits inst, Inst* data, unsigned int reg) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, reg, SZ_WORD);
+	data->dest = new_dest(OP_I_REG, inst.r->rd, SZ_WORD, MEM_ALIGN_FORCE);
 }
 
-void i_divu(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_DIV;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 0;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
+// mthi, mtlo
+void r_type_6(unsigned int code, Inst_Bits inst, Inst* data, unsigned int reg) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, inst.r->rs, SZ_WORD);
+	data->dest = new_dest(OP_I_REG, reg, SZ_WORD, MEM_ALIGN_FORCE);
 }
 
-void i_mult(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MULT;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 1;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
+// movf, movt
+void r_type_7(unsigned int code, Inst_Bits inst, Inst* data) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, inst.r->rs, SZ_WORD);
+	data->op_2 = new_operand(OP_C_FLAG, unsigned int(inst.b & 0x001c0000), SZ_WORD);
+	data->dest = new_dest(OP_I_REG, inst.r->rd, SZ_WORD, MEM_ALIGN_FORCE);
 }
 
-void i_multu(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MULT;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 0;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
+// div, divu, mult, multu
+void r_type_8(unsigned int code, Inst_Bits inst, Inst* data, unsigned int overflow) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, inst.r->rs, SZ_WORD);
+	data->op_2 = new_operand(OP_I_REG, inst.r->rt, SZ_WORD);
+	data->dest = new_dest(OP_I_REG, R_HI, SZ_DOUBLE, MEM_ALIGN_FORCE);
+	data->overflow = overflow;
 }
 
-void i_mul(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MUL;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 0;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_madd(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MADD;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 1;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_maddu(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MADD;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 0;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_msub(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MSUB;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 1;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_msubu(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MSUB;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 0;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_nor(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_NOR;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_sll(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SLL;
-	alu_data->rs = bits_12_to_16;
-	alu_data->sh = bits_22_to_26;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_SH_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_sllv(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SLL;
-	alu_data->rs = bits_12_to_16;
-	alu_data->rt = bits_7_to_11;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_sra(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SRA;
-	alu_data->rs = bits_12_to_16;
-	alu_data->sh = bits_22_to_26;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_SH_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_srav(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SRA;
-	alu_data->rs = bits_12_to_16;
-	alu_data->rt = bits_7_to_11;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_srl(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SRL;
-	alu_data->rs = bits_12_to_16;
-	alu_data->sh = bits_22_to_26;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_SH_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_srlv(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SRL;
-	alu_data->rs = bits_12_to_16;
-	alu_data->rt = bits_7_to_11;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_sub(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SUB;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 1;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_subu(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SUBU;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->overflow = 0;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_xor(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_XOR;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_slt(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SLT;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_sltu(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_SLTU;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_jalr(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_JALR;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rd = bits_17_to_21;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_jr(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_JR;
-	alu_data->rs = bits_7_to_11;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_teq(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_T;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_tne(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_T;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_tge(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_T;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_tgeu(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_T;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_tlt(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_T;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_tltu(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_T;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_mfhi(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MFHI;
-	alu_data->rd = bits_17_to_21;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_mflo(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MFLO;
-	alu_data->rd = bits_17_to_21;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_mthi(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MTHI;
-	alu_data->rs = bits_7_to_11;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_mtlo(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MTLO;
-	alu_data->rs = bits_7_to_11;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 0;
-}
-
-void i_mfc0(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MOVC;
-	alu_data->rs = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_mtc0(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MOVC;
-	alu_data->rs = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_movn(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MOVN;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_movz(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	alu_data->opcode = ALU_MOVZ;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rt = bits_12_to_16;
-	alu_data->rd = bits_17_to_21;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_movf(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	unsigned int cc = unsigned int(inst & 0x001c0000);
-	alu_data->opcode = ALU_MOVF;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rd = bits_17_to_21;
-	alu_data->cc = cc;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
-}
-
-void i_movt(unsigned int inst, P_ID* p_id, ALU_Data* alu_data, int* branch) {
-	unsigned int cc = unsigned int(inst & 0x001c0000);
-	alu_data->opcode = ALU_MOVT;
-	alu_data->rs = bits_7_to_11;
-	alu_data->rd = bits_17_to_21;
-	alu_data->cc = cc;
-	alu_data->src = ALU_REG_SRC;
-	alu_data->fp_op = 0;
-
-	p_id->mem_rd = 0;
-	p_id->mem_wr = 0;
-	p_id->reg_wr = 1;p_id->fp_wr = 0;
-	p_id->rd = alu_data->rd;
+// madd, maddu, msub, msubu
+void r_type_8(unsigned int code, Inst_Bits inst, Inst* data, unsigned int overflow) {
+	data->opcode = code;
+	data->op_1 = new_operand(OP_I_REG, inst.r->rs, SZ_WORD);
+	data->op_2 = new_operand(OP_I_REG, inst.r->rt, SZ_WORD);
+	data->op_2 = new_operand(OP_I_REG, R_HI, SZ_DOUBLE);
+	data->dest = new_dest(OP_I_REG, R_HI, SZ_DOUBLE, MEM_ALIGN_FORCE);
+	data->overflow = overflow;
 }
